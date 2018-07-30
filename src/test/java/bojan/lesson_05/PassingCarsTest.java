d25a7b80fa4d621a957804691d7769b905527408
Bojan Dimovski <gjavolce@gmail.com>
2018-07-30 23:29:29 +0200

    setup empty classes for task05

---
+++ src/test/java/bojan/lesson_05/PassingCarsTest.java
@@ -0,0 +1,34 @@
+package bojan.lesson_05;
+
+import bojan.lesson_04.MaxCounters;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sun.jvm.hotspot.utilities.Assert;
+
+import java.util.Arrays;
+
+public class PassingCarsTest {
+
+    private MaxCounters maxCounters;
+
+    @BeforeEach
+    void setUp() {
+        maxCounters = new MaxCounters();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void test_01() {
+
+    }
+
+    @Test
+    void test_02() {
+
+    }
+
+}

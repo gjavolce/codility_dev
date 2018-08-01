6328563f15b279145312df857aac61615895df9e
Bojan Dimovski <gjavolce@gmail.com>
2018-08-01 19:35:49 +0200

    lesson05 task03 solution + test

--- src/main/java/bojan/lesson_05/GenomicRangeQuery.java
+++ src/main/java/bojan/lesson_05/GenomicRangeQuery.java
@@ -60,37 +60,141 @@ package bojan.lesson_05;
 
 public class GenomicRangeQuery {
 
-
     public int[] solution(String S, int[] P, int[] Q) {
 
-        int[] weights = new int[S.length() + 1];
+        //used jagged array to hold the prefix sums of each A, C and G genoms
+        //we don't need to get prefix sums of T, you will see why.
+        int[][] genoms = new int[3][S.length() + 1];
+
+        //if the char is found in the index i, then we set it to be 1 else they are 0
+        //3 short values are needed for this reason
+        short a, c, g;
+
+        for (int i = 0; i < S.length(); i++) {
+            a = 0;
+            c = 0;
+            g = 0;
+            if ('A' == (S.charAt(i))) {
+                a = 1;
+            }
+            if ('C' == (S.charAt(i))) {
+                c = 1;
+            }
+            if ('G' == (S.charAt(i))) {
+                g = 1;
+            }
+            //here we calculate prefix sums. To learn what's prefix sums look at here https://codility.com/media/train/3-PrefixSums.pdf
+            genoms[0][i + 1] = genoms[0][i] + a;
+            genoms[1][i + 1] = genoms[1][i] + c;
+            genoms[2][i + 1] = genoms[2][i] + g;
+            //System.out.println("[" + genoms[0][i + 1] + "]" + "[" + genoms[1][i + 1] + "]" + "[" + genoms[2][i + 1] + "]");
+        }
+
+        int[] result = new int[P.length];
+        //here we go through the provided P[] and Q[] arrays as intervals
+        for (int i = 0; i < P.length; i++) {
+
+            int fromIndex = P[i];
+            //we need to add 1 to Q[i],
+            //because our genoms[0][0], genoms[1][0] and genoms[2][0]
+            //have 0 values by default, look above genoms[0][i+1] = genoms[0][i] + a;
+
+            int toIndex = Q[i] + 1;
+
+            if (genoms[0][toIndex] - genoms[0][fromIndex] > 0) {
+                result[i] = 1;
+            } else if (genoms[1][toIndex] - genoms[1][fromIndex] > 0) {
+                result[i] = 2;
+            } else if (genoms[2][toIndex] - genoms[2][fromIndex] > 0) {
+                result[i] = 3;
+            } else {
+                result[i] = 4;
+            }
+
+        }
+
+        return result;
+    }
+
+    public int[] solution2(String S, int[] P, int[] Q) {
+
+        int[][] weights = new int[S.length() + 1][4];
+
+        int[] result = new int[P.length];
 
 
         for (int i = 0; i < S.length(); i++) {
-            weights[i + 1] = weights[i] + getWeight(S.charAt(i));
+
+            for (int j = 0; j < 4; j++) {
+                weights[i + 1][j] = weights[i][j];
+            }
+            weights[i + 1][getWeight(S.charAt(i))] = weights[i][getWeight(S.charAt(i))] + 1;
         }
 
-        for(int i = 0; i < P.length; i++) {
-            System.out.println(weights[Q[i]] - weights[P[i]]);
+        for (int i = 0; i < P.length; i++) {
+            if (P[i] == Q[i]) {
+                result[i] = getWeight(S.charAt(P[i])) + 1;
+            } else {
+                result[i] = minElement(P[i], Q[i], weights) + 1;
+            }
         }
 
-        return weights;
+        return result;
 
     }
+
 
     private int getWeight(char c) {
         switch (c) {
             case 'A':
-                return 1;
-            case 'c':
-                return 2;
-            case 'G':
-                return 3;
-            case 'T':
-                return 4;
-            default:
                 return 0;
+            case 'C':
+                return 1;
+            case 'G':
+                return 2;
+            case 'T':
+                return 3;
+            default:
+                return -1;
         }
+    }
+
+    private int minElement(int p, int q, int[][] weights) {
+
+        int min = Integer.MAX_VALUE;
+        int result = -1;
+
+        System.out.print("Analayzing ending sequence Q " + q + " : ");
+        for (int i = 0; i < 4; i++) {
+            System.out.print("[" + weights[q + 1][i] + "]");
+
+
+        }
+
+        System.out.print(" and sequence P: " + p + " : ");
+        for (int i = 0; i < 4; i++) {
+            System.out.print("[" + weights[p + 1][i] + "]");
+
+
+        }
+
+        System.out.println();
+
+
+        for (int i = 3; i >= 0; i--) {
+
+            if (weights[q + 1][i] > weights[p][i] + 1) {
+
+                result = i;
+
+            }
+
+        }
+
+        System.out.println("Minimal impact factor: " + result);
+
+
+        return result;
     }
 
 }
--- src/test/java/bojan/lesson_05/GenomicRangeQueryTest.java
+++ src/test/java/bojan/lesson_05/GenomicRangeQueryTest.java
@@ -24,7 +24,15 @@ public class GenomicRangeQueryTest {
     void test_01() {
 
         Assert.that(Arrays.equals(genomicRangeQuery.solution("CAGCCTA", new int[]{2, 5, 0},
-                new int[]{4, 5, 6}), new int[]{2, 4, 5}), "ok");
+                new int[]{4, 5, 6}), new int[]{2, 4, 1}), "ok");
+
+    }
+
+//    @Test
+    void test_02() {
+
+        Assert.that(Arrays.equals(genomicRangeQuery.solution("AC", new int[]{0,0,1},
+                new int[]{0,1,1}), new int[]{1,1,2}), "ok");
 
     }
 }
